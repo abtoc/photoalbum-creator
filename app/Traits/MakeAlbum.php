fcb16a06356744f79b6fd0b88567b4478f9e6037
Toshio Abe <abtoc88@gmail.com>
2023-05-20 22:56:01 +0900

    ログの出力を変更

--- app/Traits/MakeAlbum.php
+++ app/Traits/MakeAlbum.php
@@ -17,27 +17,27 @@ trait MakeAlbum
      */
     private function collectPhoto(Album $album, String $current): void
     {
-        Log::info('Collecting photos start. album_id='.$album->id);
+        Log::info('Collecting photos start.', ['album_id' => $album->id]);
         $path = $current.'/images/cover.jpg';
-        Log::info('Collecting photo start. album_id='.$album->id.',path='.$path);
+        Log::info('Collecting photo start.', ['album_id' => $album->id, 'path' => $path]);
         Storage::disk('local')->writeStream(
             $path,
             Storage::disk('s3')->readStream($album->getCoverPath())
         );
-        Log::info('Collected photo terminate. album_id='.$album->id.'path='.$path);
+        Log::info('Collected photo terminate.', ['album_id' => $album->id, 'path' => $path]);
 
         $query = $album->pages()
                     ->orderBy('page', 'asc');
         foreach($query->cursor() as $page){
             $path = sprintf("%s/images/%05d.jpg", $current, $page->page);
-            Log::info('Collecting photo start. album_id='.$album->id.',path='.$path);
+            Log::info('Collecting photo start.', ['album_id' => $album->id, 'path' => $path]);
             Storage::disk('local')->writeStream(
                 $path,
                 Storage::disk('s3')->readStream($page->photo->getPath())
             );
-            Log::info('Collected photo terminate. album_id='.$album->id.'path='.$path);
+            Log::info('Collected photo terminate.', ['album_id' => $album->id, 'path' => $path]);
         }
-        Log::info('Collected photo terminate. album_id='.$album->id);
+        Log::info('Collected photo terminate.', ['album_id' => $album->id]);
     }
 
     /**
@@ -45,7 +45,7 @@ trait MakeAlbum
      */
     private function makeXML(string $path, $content)
     {
-        Log::info('Add epub file. path='.$path);
+        Log::info('Add epub file.', ['path' => $path]);
         Storage::disk('local')->put($path, $content);
     }
 
@@ -54,7 +54,7 @@ trait MakeAlbum
      */
     private function makeXMLs(Album $album, String $current): void
     {
-        Log::info('Adding epub files. album_id='.$album->id);
+        Log::info('Adding epub files.', ['album_id' => $album->id]);
         $this->makeXML($current.'/mimetype', 'application/epub+zip');
         $this->makeXML($current.'/META-INF/container.xml', view('epub.container'));
 
@@ -64,22 +64,22 @@ trait MakeAlbum
         foreach($album->pages()->orderBy('page', 'asc')->cursor() as $page){
             $this->makeXML($current.'/text/part'.sprintf('%05d', $page->page).'.xhtml', view('epub.section', ['page' => $page]));
         }
-        Log::info('Added epub files. album_id='.$album->id);
+        Log::info('Added epub files.', ['album_id' => $album->id]);
     }
 
     public function make(Album $album)
     {
-        Log::info('Maiking album start. album_id='.$album->id.',email='.$album->user->email);
+        Log::info('Maiking album start.', ['album_id' => $album->id, 'email' => $album->user->email]);
 
         $current = sprintf('albums/%08d', $album->id);
         if(Storage::disk('local')->exists($current)){
             Storage::disk('local')->deleteDirectory($current);
-            Log::info('Delete epub working directory. album_id='.$album->id.',path='.$current);
+            Log::info('Delete epub working directory.', ['album_id' => $album->id, 'path' => $current]);
         }
         Storage::disk('local')->makeDirectory($current);
         if(Storage::disk('local')->exists($current.'.epub')){
             Storage::disk('local')->delete($current.'.epub');
-            Log::info('Delete epub working file. album_id='.$album->id.',path='.$current.'.epub');
+            Log::info('Delete epub working file.', ['album_id' => $album->id,  'path' => $current.'.epub']);
         }
 
         $this->collectPhoto($album, $current);
@@ -87,28 +87,28 @@ trait MakeAlbum
 
         $path = Storage::disk('local')->path($current);
         chdir($path);
-        $command = '/usr/bin/zip -0 -X '.$path.'.epub mimetype'; 
-        Log::info('Making epub file. album_id='.$album->id.',path='.$path.'.'.'epub,command='.$command);
-        $result = exec($command, $output);
-        Log::info('Maked epub file. album_id='.$album->id.',path='.$path.'.epub,command='.$command.',result='.$result.':'.implode(PHP_EOL, $output));
-        $command = '/usr/bin/zip -r '.$path.'.epub * -x mimetype'; 
-        Log::info('Making epub file. album_id='.$album->id.',path='.$path.'.epub,command='.$command);
-        $result = exec($command, $output);
-        Log::info('Maked epub file. album_id='.$album->id.',path='.$path.'.epub,command='.$command.',result='.$result.':'.implode(PHP_EOL, $output));
+        $command = '/usr/bin/zip -0 -X '.$path.'.epub mimetype 2>&1'; 
+        Log::info('Making epub file.', ['album_id' =>  $album->id, 'path' => $path.'.epub' ,'command' => $command]);
+        exec($command, $output, $result);
+        Log::info('Maked epub file.', ['album_id' => $album->id, 'path' => $path.'.epub', 'command' =>$command, 'result' => $result, 'output' => implode(PHP_EOL, $output)]);
+        $command = '/usr/bin/zip -r '.$path.'.epub * -x mimetype 2>&1'; 
+        Log::info('Making epub file.', ['album_id' => $album->id, 'path' => $path.'.epub', 'command' => $command]);
+        exec($command, $output, $result);
+        Log::info('Maked epub file.', ['album_id' => $album->id, 'path' => $path.'.epub', 'command' => $command, 'result' => $result, 'output' => implode(PHP_EOL, $output)]);
 
-        Log::info('Uploading epub file. album_id='.$album->id.',path='.$current.'.epub');
+        Log::info('Uploading epub file.', ['album_id' => $album->id, 'path' => $current.'.epub']);
         Storage::disk('s3')->writeStream(
             $album->getEpubPath(),
             Storage::disk('local')->readStream($current.'.epub'),
         );
-        Log::info('Uploaded epub file. album_id='.$album->id.',path='.$current.'.epub');
+        Log::info('Uploaded epub file.', ['album_id' => $album->id, ',path' => $current.'.epub']);
 
         Storage::disk('local')->deleteDirectory($current);
-        Log::info('Delete epub working directory. album_id='.$album->id.',path='.$current);
+        Log::info('Delete epub working directory.', ['album_id' => $album->id, 'path' => $current]);
         Storage::disk('local')->delete($current.'.epub');
-        Log::info('Delete epub working file. album_id='.$album->id.',path='.$current.'.epub');
+        Log::info('Delete epub working file.', ['album_id' => $album->id, 'path' => $current.'.epub']);
 
-        Log::info('Maked album terminate. album_id='.$album->id.',email='.$album->user->email);
+        Log::info('Maked album terminate.', ['album_id' => $album->id, 'email' => $album->user->email]);
         return 0;
     }
 };
