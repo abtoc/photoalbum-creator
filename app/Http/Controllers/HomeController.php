884981e2898b1346cf4ad3be92f8a60b06dc95d1
Toshio Abe <abtoc88@gmail.com>
2023-05-21 00:18:47 +0900

    ホーム画面をある程度作る

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $overview = (object)[
+            'photo_count' => Auth::user()->photos()->count(),
+            'album_count' => Auth::user()->albums()->count(),
+            'capacity' => Auth::user()->getCapacity(),
+            'used_capacity' => Auth::user()->getUsedCapacity(),
+        ];
+        return view('home', [
+            'overview' => $overview,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,41 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center pt-2">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Notice') }}</div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center pt-4">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">{{ __('Overview') }}</div>
+
+                <div class="card-body">
+                    <ul>
+                        <li>{{ sprintf(__('%d photos.'), $overview->photo_count) }}</li>
+                        <li>{{ sprintf(__('%d albums.'), $overview->album_count) }}</li>
+                        <li>{{ sprintf(__('%s out of %s use.'), byte_to_unit($overview->capacity), byte_to_unit($overview->used_capacity)) }}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">{{ __('Activity') }}</div>
+    
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
+    
                     {{ __('You are logged in!') }}
                 </div>
             </div>
